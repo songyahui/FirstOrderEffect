ec22e3266b401d1b0fde11720108465abc4cc648
songyahui <songyahuiapp@hotmail.com>
2022-03-15 16:59:48 +0800

    m

--- rewriteDelimited/reorder.ml
+++ rewriteDelimited/reorder.ml
@@ -9,16 +9,11 @@ let rec take lst n =
     perform (Found (first, rest))
   else first :: (take rest (n - 1))
   
-
 let handler lst n =
   match take lst n with
   | li -> li 
   | effect (Found (first,  rest)) k -> first :: continue k rest
 
 let main = 
-  let list = handler [1;2;3;4;0] 1 in 
+  let list = handler [1;2;3;4;0] 3 in 
   print_string (List.fold_left (fun acc a -> acc ^ " " ^ string_of_int a) "" list ^ "\n")
-
-
-(*     shift (fun k -> first :: k rest)
- *)
